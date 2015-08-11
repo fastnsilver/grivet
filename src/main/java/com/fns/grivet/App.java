609bca2b6c448aa80b82c6719adf9a7770d75e22
Chris Phillipson <fastnsilver@gmail.com>
2015-08-11 10:32:32 -0700

    Upgrade to Spring Boot 1.3.0.M3 * H2 web console is auto-configured now, so remove @Bean from App.java * spring-data-releasetrain.version updated to Gosling-RC1

--- src/main/java/com/fns/grivet/App.java
+++ src/main/java/com/fns/grivet/App.java
@@ -15,12 +15,8 @@
  */
 package com.fns.grivet;
 
-import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
 /**
  * Bootstraps the application
@@ -34,11 +30,4 @@ public class App {
         SpringApplication.run(App.class, args);
     }
 
-    @Bean
-    @ConditionalOnProperty(name="spring.datasource.driver-class-name", havingValue="org.h2.Driver", matchIfMissing=false)
-    ServletRegistrationBean h2servletRegistration(){
-        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
-        registrationBean.addUrlMappings("/h2/console/*");
-        return registrationBean;
-    }
 }
