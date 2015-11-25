63dfbdf515f012839cdb65872b062be1bf2c0cfc
Chris Phillipson <fastnsilver@gmail.com>
2015-11-25 12:17:24 -0800

    Get Spring Boot Admin working
    * adjust configuration and add appropriate dependencies

--- support/boot-admin/src/main/java/com/fns/grivet/services/admin/App.java
+++ support/boot-admin/src/main/java/com/fns/grivet/services/admin/App.java
@@ -17,6 +17,7 @@ package com.fns.grivet.services.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
 
@@ -27,6 +28,7 @@ import de.codecentric.boot.admin.config.EnableAdminServer;
  */
 @SpringBootApplication
 @EnableAdminServer
+@EnableDiscoveryClient
 public class App {
     
     public static void main(String[] args) {
