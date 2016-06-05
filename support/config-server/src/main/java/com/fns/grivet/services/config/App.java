2d9d452305a369861e13099034e0a329370eff85
Chris Phillipson <fastnsilver@gmail.com>
2016-06-05 09:57:09 -0700

    Take some hints from... * https://github.com/kbastani/spring-cloud-event-sourcing-example as far as how to provision edge-service, discovery-service, and config-service * still need to refactor endpoint uris to unify api behind edge-service

--- support/config-server/src/main/java/com/fns/grivet/services/config/App.java
+++ support/config-server/src/main/java/com/fns/grivet/services/config/App.java
@@ -17,12 +17,10 @@ package com.fns.grivet.services.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableDiscoveryClient
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
