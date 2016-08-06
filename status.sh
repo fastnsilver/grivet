8e0914e30675a335b21caaf96516de9515568bc7
Chris Phillipson <fastnsilver@gmail.com>
2016-08-06 15:19:18 -0700

    Upgrade to Spring Boot 1.3.5 and Spring Cloud Brixton SR4
    Also add some scripts for trobleshooting Docker-Compose deployments

---
+++ status.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ $# -ne 1 ]; then
+    echo "Usage: ./status.sh standalone|pipeline"
+    exit 1
+fi
+
+suffix=$1
+
+# Change directories
+cd docker
+
+# Export the active docker machine IP
+export DOCKER_IP=$(docker-machine ip $(docker-machine active))
+
+# docker-machine doesn't exist in Linux, assign default ip if it's not set
+DOCKER_IP=${DOCKER_IP:-0.0.0.0}
+
+# Display status of cluster
+docker-compose -f docker-compose.yml -f docker-compose-$suffix.yml ps
