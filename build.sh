1eb734a82bbeb42ee701d0c6759ffa8ae5fb6990
Chris Phillipson <fastnsilver@gmail.com>
2016-08-23 05:05:58 -0700

    Forgot to add dashboard to list of directories

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-directories=( "core/deployables/admin" "core/deployables/ingest" "core/deployables/persist" "core/deployables/query" "core/deployables/standalone" "support/boot-admin" "support/config-server" "support/api-gateway" "support/discovery-service")
+directories=( "core/deployables/admin" "core/deployables/ingest" "core/deployables/persist" "core/deployables/query" "core/deployables/standalone" "support/boot-admin" "support/config-server" "support/api-gateway" "support/discovery-service" "support/dashboard")
 
 for i in "${directories[@]}"
 do
