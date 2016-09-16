7104689d073ee5dc324f70f2e09924fb7ea4ba68
Chris Phillipson <fastnsilver@gmail.com>
2016-09-15 18:37:50 -0700

    Add the capability to PATCH an entity's attribute-value(s)
    Also update error response to be more expressive (w/ errors)

--- core/utils/src/main/java/com/fns/grivet/service/SchemaValidationException.java
+++ core/utils/src/main/java/com/fns/grivet/service/SchemaValidationException.java
@@ -1,11 +1,23 @@
 package com.fns.grivet.service;
 
+import java.util.List;
+
 public class SchemaValidationException extends RuntimeException {
 
-    private static final long serialVersionUID = 1L;
-    
-    public SchemaValidationException(String message, Throwable t) {
-        super(message, t);
-    }
+	private static final long serialVersionUID = 1L;
+
+	private List<Object> processingFailures;
+
+	public SchemaValidationException(String message, Throwable t) {
+		super(message, t);
+	}
+
+	public SchemaValidationException(List<Object> processingFailures) {
+		this.processingFailures = processingFailures;
+	}
+
+	public List<Object> getProcessingFailures() {
+		return processingFailures;
+	}
 
 }
