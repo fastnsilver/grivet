e312827658d8e396a2f8f37f10c67e85bd340dc8
Chris Phillipson <fastnsilver@gmail.com>
2017-09-06 07:26:05 -0700

    Actuator updates * Restore actuator (production-ready) endpoints; @see https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-2.0.0-M4-Release-Notes#actuator-1 * Refine MetricsAspect impl to define pointcuts for each REST operation

--- core/common/src/main/java/com/fns/grivet/controller/MetricsAspect.java
+++ core/common/src/main/java/com/fns/grivet/controller/MetricsAspect.java
@@ -5,6 +5,7 @@ import java.lang.reflect.Method;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -22,11 +23,44 @@ class MetricsAspect {
 	public MetricsAspect(MetricRegistry metricRegistry) {
 		this.metricRegistry = metricRegistry;
 	}
+	
+	@Pointcut("execution(* com.fns.grivet.controller.*Controller.*(..))")
+	private void controller() {}
+	
+	@Pointcut("@annotation(org.springframework.web.bind.annotation.DeleteMapping)")
+	private void deleteMapping() {}
+	
+	@Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+    private void getMapping() {}
+	
+	@Pointcut("@annotation(org.springframework.web.bind.annotation.PostMapping)")
+    private void postMapping() {}
+	
+	@Pointcut("@annotation(org.springframework.web.bind.annotation.PutMapping)")
+    private void putMapping() {}
 
-	@Around("execution(* com.fns.grivet.controller.*Controller.*(..)) && @annotation(org.springframework.web.bind.annotation.RequestMapping)")
-	public Object run(ProceedingJoinPoint pjp) throws Throwable {
+	@Around("controller() && deleteMapping()")
+	public Object delete(ProceedingJoinPoint pjp) throws Throwable {
+		return instrument(pjp);
+	}
+	
+	@Around("controller() && getMapping()")
+    public Object get(ProceedingJoinPoint pjp) throws Throwable {
+        return instrument(pjp);
+    }
+	
+	@Around("controller() && postMapping()")
+    public Object post(ProceedingJoinPoint pjp) throws Throwable {
+        return instrument(pjp);
+    }
+	
+	@Around("controller() && putMapping()")
+    public Object put(ProceedingJoinPoint pjp) throws Throwable {
+        return instrument(pjp);
+    }
 
-		Class<?> curClass = pjp.getTarget().getClass();
+    private Object instrument(ProceedingJoinPoint pjp) throws Throwable, Exception {
+        Class<?> curClass = pjp.getTarget().getClass();
 		MethodSignature ms = (MethodSignature) pjp.getSignature();
 		Method m = ms.getMethod();
 
@@ -37,7 +71,7 @@ class MetricsAspect {
 		} catch (Exception t) {
 			throw t;
 		}
-	}
+    }
 
 }
 
