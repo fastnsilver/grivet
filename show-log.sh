a98473197fbc23dd2c17398797f850c8367f5b77
Chris Phillipson <fastnsilver@gmail.com>
2017-09-19 07:32:28 -0700

    Work in progress to support Docker Compose with Finchley * Remove all traces of docker profile * Fold in Eureka and Config Client settings to docker-compose YAML file variants * Moved all individual application*.yml config to {application}.yml files https://github.com/fastnsilver/config-repo * Prune all redundant config; global config (default profile) handled in application.yml in https://github.com/fastnsilver/config-repo * Adjust startup, shutdown, status, and show-log scripts * Docker Compose file service version updates and available heap memory increased for Java apps

--- show-log.sh
+++ show-log.sh
@@ -10,14 +10,22 @@ fi
 suffix=$1
 docker_image=$2
 
-# Change directories
-cd docker
-
 # Export the active docker machine IP
 export DOCKER_IP=$(docker-machine ip $(docker-machine active))
 
+if [ -z "$DOCKER_IP" ]; then
+	SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+	if [ -f "$SCRIPT_DIR/.local" ]; then
+		export DOCKER_IP="127.0.0.1"
+	fi
+fi
+
 # docker-machine doesn't exist in Linux, assign default ip if it's not set
 DOCKER_IP=${DOCKER_IP:-0.0.0.0}
+echo "Docker IP is $DOCKER_IP"
+
+# Change directories
+cd docker
 
 # Display status of cluster
 docker-compose -f docker-compose.yml -f docker-compose-$suffix.yml logs $docker_image
