b3ffa6cff5651a045806309a99d8f6c739b2c031
Chris Phillipson <fastnsilver@gmail.com>
2018-03-05 14:52:49 -0800

    Update to Spring Boot 2.0.0.RELEASE * Minor updates to configuration and dependencies

--- core/repositories/src/main/java/com/fns/grivet/config/DataSourceConfig.java
+++ core/repositories/src/main/java/com/fns/grivet/config/DataSourceConfig.java
@@ -15,58 +15,18 @@
  */
 package com.fns.grivet.config;
 
-import java.util.Collection;
-
-import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.metrics.jdbc.DataSourcePoolMetrics;
-import org.springframework.boot.jdbc.metadata.DataSourcePoolMetadataProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.fns.grivet.repo.AuditorProvider;
 
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Tags;
-
 @Configuration
 @EnableTransactionManagement
 @EnableJpaAuditing(auditorAwareRef="auditorProvider")
 public class DataSourceConfig {
-    
-    @Autowired
-    private DataSource dataSource;
-
-    @Autowired
-    private Collection<DataSourcePoolMetadataProvider> metadataProviders;
-
-    @Autowired
-    private MeterRegistry registry;
-
-    @Autowired
-    private Environment env;
-
-    @PostConstruct
-    private void instrumentDataSource() {
-        new DataSourcePoolMetrics(
-            dataSource,
-            metadataProviders,
-            "data.source",
-            Tags.zip(
-                    "stack", 
-                    env.acceptsProfiles("h2") ? "in-memory" : "external-datasource")
-        ).bindTo(registry);
-    }
-    
-    
-    
-    
         
     @Bean
     public AuditorAware<String> auditorProvider() {
