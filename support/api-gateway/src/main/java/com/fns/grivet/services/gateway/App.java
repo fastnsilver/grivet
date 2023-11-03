287f87b73dfa5a3577ede2fab02967c176aaaa46
Chris Phillipson <cphillipson@pivotal.io>
2023-11-02 17:10:40 -0700

    Swap API Gateway: Phase out Zuul, bring in Spring Cloud Gateway
    * taking inspiration from https://spring.io/guides/gs/gateway/

--- support/api-gateway/src/main/java/com/fns/grivet/services/gateway/App.java
+++ support/api-gateway/src/main/java/com/fns/grivet/services/gateway/App.java
@@ -18,10 +18,8 @@ package com.fns.grivet.services.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulProxy
 @EnableDiscoveryClient
 public class App {
 	public static void main(String[] args) {
