81572d61841f717caeadd4622573650ba00599d1
Chris Phillipson <cphillipson@pivotal.io>
2023-11-03 10:06:22 -0700

    Rename observability module and remove shared-config/application.yml
    * because it will be sourced from a Git repo along with individual configuration for each Spring Boot microservice via a Spring Cloud Config Server instance
    * adjust Docker Compose configuration

--- support/microservices-console/src/main/java/com/fns/grivet/services/console/App.java
+++ support/microservices-console/src/main/java/com/fns/grivet/services/console/App.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.fns.grivet.services.observability;
+package com.fns.grivet.services.console;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
