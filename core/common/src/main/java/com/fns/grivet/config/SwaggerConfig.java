4107ec3cca7ffd722a8cb22ac2374efe62d2cc97
Chris Phillipson <cphillipson@pivotal.io>
2023-11-24 05:31:17 -0800

    Upgrade spring-restdocs and httpclient5 versions

--- core/common/src/main/java/com/fns/grivet/config/SwaggerConfig.java
+++ core/common/src/main/java/com/fns/grivet/config/SwaggerConfig.java
@@ -33,7 +33,7 @@ public class SwaggerConfig {
 
 	private ApiInfo apiInfo(String projectName, String projectDescription) {
 		var apiInfo = new ApiInfo("%s REST API".formatted(projectName), projectDescription, "v1",
-				"https://github.com/fastnsilver/grivet/blob/master/TERMS",
+				"https://github.com/fastnsilver/grivet/blob/main/TERMS",
 				new Contact("Chris Phillipson", "http://techblitz.io/grivet/", "fastnsilver@gmail.com"),
 				"Apache License 2.0", "https://www.apache.org/licenses/LICENSE-2.0", new ArrayList<VendorExtension>());
 		return apiInfo;
